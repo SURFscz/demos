ebaaa54f54613e6b5f5a0c43dec68fc5c37c756d
Martin van Es <martin@mrvanes.com>
2021-06-02 12:57:18 +0200

    Add ReverseProxy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # demos
+## ReverseProxy
+An example of how Apache can be used a an authenticating RP for JupyterHub and Etherpad
---
+++ ReverseProxy/jupyterhub/jupyterhub2_config.py
@@ -0,0 +1,20 @@
+"""
+jupyterhub_config.py-docker
+---------------------------
+
+Configuration for JupyterHub tutorial - used for docker deployment
+"""
+
+# User whitelist - set of users allowed to use the Hub
+c.Authenticator.allowed_users = {'admin', 'user1', 'user2', 'user3'}
+
+# Administrators - set of users who can administer the Hub itself
+c.Authenticator.admin_users = {'admin'}
+
+# Authenticator
+c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator.remote_user_auth.RemoteUserLocalAuthenticator'
+c.RemoteUserLocalAuthenticator.header_name = "X-Authenticated-User"
+
+c.LocalAuthenticator.create_system_users = True
+
+c.JupyterHub.base_url = '/hub4/'
