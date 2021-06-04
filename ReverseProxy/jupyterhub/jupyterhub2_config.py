89a8b83dce6c68f60757ec7054b1573dca192b9c
Martin <martin.vanes@surf.nl>
2021-06-04 17:32:42 +0200

    JH and EP based on OIDC sub

--- ReverseProxy/jupyterhub/jupyterhub2_config.py
+++ ReverseProxy/jupyterhub/jupyterhub2_config.py
@@ -6,15 +6,16 @@ Configuration for JupyterHub tutorial - used for docker deployment
 """
 
 # User whitelist - set of users allowed to use the Hub
-c.Authenticator.allowed_users = {'admin', 'user1', 'user2', 'user3'}
+#c.Authenticator.allowed_users = {'admin', 'user1', 'user2', 'user3'}
 
 # Administrators - set of users who can administer the Hub itself
-c.Authenticator.admin_users = {'admin'}
+#c.Authenticator.admin_users = {'admin'}
+
+#c.LocalAuthenticator.create_system_users = True
 
 # Authenticator
 c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator.remote_user_auth.RemoteUserLocalAuthenticator'
 c.RemoteUserLocalAuthenticator.header_name = "X-Authenticated-User"
-
-c.LocalAuthenticator.create_system_users = True
+c.JupyterHub.spawner_class = 'jupyterhub.spawner.SimpleLocalProcessSpawner'
 
 c.JupyterHub.base_url = '/hub4/'
